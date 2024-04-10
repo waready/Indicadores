74fe94bb03bb36a5debffe526852d9f29c8a7a53
waready <elbarto119@hotmail.com>
2024-04-10 14:54:44 -0500

    indicadores

---
+++ config.js
@@ -0,0 +1,44 @@
+/**
+ * Author: Antony Japura Paredes
+ * Email: antonyjapura11@gmail.com
+ * Created at: 2023-06-06 06:06
+ */
+
+// Initialize variables globales 
+// BTEXPOSER ON ("true") - OFF ("false")
+
+window.GetBTexposer= "true"
+
+// url ambiente - ejemplo:
+/*
+    url ambiente = http://appjava2019:10222/desaprodv2r3orcl/servlet/com.dlya.bantotal.odwsbt_Authenticate_v1
+    GetUrlPro = http://pioneroapp:6004/productogx16/servlet/
+*/
+/*
+    BTEXPOSER
+    GetUrlPro = https://pioneroapp2.dlya.corp:3001/api/
+*/
+
+window.GetUrlPro= "https://miscuotasqa.bantotal.com/api/"
+
+//sufijo del ambiente - ejemplo
+/*
+    url ambiente = http://appjava2019:10222/desaprodv2r3orcl/servlet/com.dlya.bantotal.odwsbt_Authenticate_v1
+    GetSufijo = odwsbt
+*/
+/*
+    BTEXPOSER 
+    GetSufijo = ""
+*/
+window.GetSufijo= ""
+
+//Version auth 
+/*
+    url ambiente = http://appjava2019:10222/desaprodv2r3orcl/servlet/com.dlya.bantotal.odwsbt_Authenticate_v1
+    GetVrSer = v1?
+*/
+/*
+    BTEXPOSER
+    GetVrSer = v1/
+*/
+window.GetVrSer= "v1"
